e7c6a26bfd21a9193fee46b4e137f7998d46fcfd
Ruslan Hlaznov <ruslan.glaznyov@gmail.com>
2022-12-16 15:25:56 +0200

    feat: txs all (#10)
    * chore: added esNext to use `flat()` method in array
    
    * feat: txs all
    
    * chore: run lint:fix

--- integrations/uniswap/src/evm-contract-events.ts
+++ integrations/uniswap/src/evm-contract-events.ts
@@ -1,4 +1,4 @@
-import { DataItem, IRuntime, Validator, sha256 } from '@kyvejs/protocol';
+import { DataItem, IRuntime, sha256, Validator } from '@kyvejs/protocol';
 import { providers, utils } from 'ethers';
 
 // Method to get the named args
