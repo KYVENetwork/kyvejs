705c3ef6dd4f018b93408718705277eca33f56c5
Troy Kessler <troy.kessler99@gmail.com>
2023-06-13 16:31:40 +0200

    connected all runtime methods in docker runtime

--- integrations/docker/src/runtime.ts
+++ integrations/docker/src/runtime.ts
@@ -2,70 +2,93 @@ import { DataItem, IRuntime } from "@kyvejs/protocol";
 import { spawnSync } from "child_process";
 import { name, version } from "../package.json";
 
-// TendermintBSync config
-interface IConfig {
-  network: string;
-  rpc: string;
-}
-
 export default class Docker implements IRuntime {
   public name = name;
   public version = version;
 
-  async validateGetConfig(rawConfig: string): Promise<any> {
-    const config: IConfig = JSON.parse(rawConfig);
-
-    if (!config.network) {
-      throw new Error(`Config does not have property "network" defined`);
-    }
-
-    if (!config.rpc) {
-      throw new Error(`Config does not have property "rpc" defined`);
-    }
-
-    if (process.env.KYVEJS_TENDERMINT_BSYNC_RPC) {
-      config.rpc = process.env.KYVEJS_TENDERMINT_BSYNC_RPC;
-    }
-
-    return config;
+  private run(args: any[]): any {
+    spawnSync("docker", ["run", "runtime", ...args]);
   }
 
-  async getDataItem(_: any, key: string): Promise<DataItem> {
-    const result = spawnSync("docker", ["run", "runtime_test", "read", key]);
+  async validateGetConfig(rawConfig: string): Promise<any> {
+    const result = this.run(["validateGetConfig", rawConfig]);
+
     if (result.status !== 0) {
       throw new Error(result.stderr.toString());
-    } else {
-      return { key, value: JSON.parse(result.stdout.toString()) };
     }
+
+    return JSON.parse(result.stdout.toString());
   }
 
-  async prevalidateDataItem(_: any, item: DataItem): Promise<boolean> {
-    return true;
+  async getDataItem(c: any, key: string): Promise<DataItem> {
+    const result = this.run(["getDataItem", c, key]);
+
+    if (result.status !== 0) {
+      throw new Error(result.stderr.toString());
+    }
+
+    return JSON.parse(result.stdout.toString());
   }
 
-  async transformDataItem(_: any, item: DataItem): Promise<DataItem> {
-    return item;
+  async prevalidateDataItem(c: any, item: DataItem): Promise<boolean> {
+    const result = this.run(["prevalidateDataItem", c, item]);
+
+    if (result.status !== 0) {
+      throw new Error(result.stderr.toString());
+    }
+
+    return JSON.parse(result.stdout.toString());
+  }
+
+  async transformDataItem(c: any, item: DataItem): Promise<DataItem> {
+    const result = this.run(["transformDataItem", c, item]);
+
+    if (result.status !== 0) {
+      throw new Error(result.stderr.toString());
+    }
+
+    return JSON.parse(result.stdout.toString());
   }
 
   async validateDataItem(
-    _: any,
+    c: any,
     proposedDataItem: DataItem,
     validationDataItem: DataItem
   ): Promise<boolean> {
-    // apply equal comparison
-    return (
-      JSON.stringify(proposedDataItem) === JSON.stringify(validationDataItem)
-    );
+    const result = this.run([
+      "validateDataItem",
+      c,
+      proposedDataItem,
+      validationDataItem,
+    ]);
+
+    if (result.status !== 0) {
+      throw new Error(result.stderr.toString());
+    }
+
+    return JSON.parse(result.stdout.toString());
   }
 
   public async summarizeDataBundle(
-    _: any,
+    c: any,
     bundle: DataItem[]
   ): Promise<string> {
-    return "";
+    const result = this.run(["summarizeDataBundle", c, bundle]);
+
+    if (result.status !== 0) {
+      throw new Error(result.stderr.toString());
+    }
+
+    return JSON.parse(result.stdout.toString());
   }
 
-  public async nextKey(_: any, key: string): Promise<string> {
-    return (parseInt(key) + 1).toString();
+  public async nextKey(c: any, key: string): Promise<string> {
+    const result = this.run(["nextKey", c, key]);
+
+    if (result.status !== 0) {
+      throw new Error(result.stderr.toString());
+    }
+
+    return JSON.parse(result.stdout.toString());
   }
 }
