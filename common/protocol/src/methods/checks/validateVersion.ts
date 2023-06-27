297c6ee50d380e7ff2d0f9d075abf223d4f00d67
Troy Kessler <troy.kessler99@gmail.com>
2023-06-27 16:13:43 +0200

    feat: added new versioning system

--- common/protocol/src/methods/checks/validateVersion.ts
+++ common/protocol/src/methods/checks/validateVersion.ts
@@ -1,8 +1,9 @@
 import { Validator, standardizeJSON } from "../..";
+import { valid, major, minor } from "semver";
 
 /**
- * validateVersion checks if the version of the pool matches with the runtime
- * version of the node. If it does not match the node will exit.
+ * validateVersion checks if the major and minor version of the pool matches
+ * with the runtime version of the node. If it does not match the node will exit.
  *
  * @method validateVersion
  * @param {Validator} this
@@ -11,10 +12,34 @@ import { Validator, standardizeJSON } from "../..";
 export function validateVersion(this: Validator): void {
   try {
     this.logger.debug(
-      `Comparing pool runtime version with protocol node runtime version`
+      `Comparing remote runtime version with local runtime version`
     );
 
-    if (this.pool.data!.protocol!.version !== this.runtime.version) {
+    const remoteVersion = valid(this.pool.data!.protocol!.version);
+    const localVersion = valid(this.runtime.version);
+
+    // exit if remote version is invalid
+    if (remoteVersion === null) {
+      this.logger.fatal(
+        `Error while validating remote runtime version. Exiting ...`
+      );
+      this.logger.fatal(`Remote version: ${this.pool.data!.protocol!.version}`);
+
+      process.exit(1);
+    }
+
+    // exit if local version is invalid
+    if (localVersion === null) {
+      this.logger.fatal(
+        `Error while validating local runtime version. Exiting ...`
+      );
+      this.logger.fatal(`Local version: ${this.pool.data!.protocol!.version}`);
+
+      process.exit(1);
+    }
+
+    // exit if major version does not match
+    if (major(remoteVersion) !== major(localVersion)) {
       this.logger.fatal(`Running an invalid version. Exiting ...`);
       this.logger.fatal(
         `Found Runtime version = ${this.runtime.version} required = ${
@@ -24,8 +49,20 @@ export function validateVersion(this: Validator): void {
       process.exit(1);
     }
 
+    // exit if minor version does not match
+    if (minor(remoteVersion) !== minor(localVersion)) {
+      this.logger.fatal(`Running an invalid version. Exiting ...`);
+      this.logger.fatal(
+        `Found Runtime version = ${this.runtime.version} required = ${
+          this.pool.data!.protocol!.version
+        }`
+      );
+      process.exit(1);
+    }
+
+    // patch version can be different, continue in this case
     this.logger.info(
-      `Validator running on runtime version = ${this.runtime.version}`
+      `Validator running on valid runtime version = ${this.runtime.version}`
     );
   } catch (err) {
     this.logger.fatal(`Error while validating runtime version. Exiting ...`);
