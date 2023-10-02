23fcc86878280c9046d58d634d3dc4af7e22bc0c
Kostas Christopoulos <k.christopoulos@rocketfueldev.com>
2023-10-02 09:23:06 +0300

    chore: transpile docker integration runtime to TS
    - cleanup various stuff
    - apply docker and code fixes to comply with new file structur
    
    Signed-off-by: Kostas Christopoulos <k.christopoulos@rocketfueldev.com>

--- proxies/docker/src/runtime.ts
+++ proxies/docker/src/runtime.ts
@@ -1,6 +1,12 @@
 import { DataItem, IRuntime } from '@kyvejs/protocol';
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
+import { 
+  GetRuntimeNameResponse, GetRuntimeVersionResponse, ValidateSetConfigResponse,
+  GetDataItemResponse, PrevalidateDataItemResponse, TransformDataItemResponse,
+  ValidateDataItemResponse, SummarizeDataBundleResponse, NextKeyResponse
+
+ } from './proto/runtime';
 
 // config is a serialized string
 type IConfig = string;
@@ -33,7 +39,7 @@ export default class Docker implements IRuntime {
 
   async getName(): Promise<string> {
     return new Promise<string>((resolve, reject) => {
-      this.grpcClient.GetRuntimeName({}, (error: Error | null, runtimeResponse: any) => {
+      this.grpcClient.GetRuntimeName({}, (error: Error | null, runtimeResponse: GetRuntimeNameResponse) => {
         if (error) {
           reject(error);
         } else {
@@ -45,7 +51,7 @@ export default class Docker implements IRuntime {
 
   async getVersion(): Promise<string> {
     return new Promise<string>((resolve, reject) => {
-      this.grpcClient.GetRuntimeVersion({}, (error: Error | null, runtimeResponse: any) => {
+      this.grpcClient.GetRuntimeVersion({}, (error: Error | null, runtimeResponse: GetRuntimeVersionResponse) => {
         if (error) {
           reject(error);
         } else {
@@ -57,16 +63,16 @@ export default class Docker implements IRuntime {
 
   async validateSetConfig(rawConfig: string): Promise<string> {
     return new Promise<string>((resolve, reject) => {
+      console.log(rawConfig);
       this.grpcClient.ValidateSetConfig(
         {
           raw_config: rawConfig,
         },
-        (error: Error | null, runtimeResponse: any) => {
+        (error: Error | null, runtimeResponse: ValidateSetConfigResponse) => {
           if (error) {
             // Handle the error here if needed
             reject(error);
           } else {
-            // Assuming you have a valid serialized JSON string
             this.config = runtimeResponse.serialized_config;
   
             // Resolve with the correct response
@@ -79,6 +85,7 @@ export default class Docker implements IRuntime {
 
   async getDataItem(key: string): Promise<DataItem> {
     return new Promise<DataItem>((resolve, reject) => {
+      console.log(this.config);
       this.grpcClient.GetDataItem(
         {
           config: {
@@ -86,11 +93,16 @@ export default class Docker implements IRuntime {
           },
           key: key, // Use the provided 'key' parameter
         },
-        (error: Error | null, runtimeResponse: any) => {
+        (error: Error | null, runtimeResponse: GetDataItemResponse) => {
           if (error) {
             // Handle the error here if needed
             reject(error);
           } else {
+            if (runtimeResponse.data_item === undefined) {
+              throw new Error("runtimeResponse.dataItem is undefined");
+            }
+
+            console.log(runtimeResponse);
             const responseDataItem: DataItem = {
               key: runtimeResponse.data_item.key,
               value: JSON.parse(runtimeResponse.data_item.value)
@@ -116,7 +128,7 @@ export default class Docker implements IRuntime {
           },
           data_item: request_item,
         },
-        (error: Error | null, runtimeResponse: any) => {
+        (error: Error | null, runtimeResponse: PrevalidateDataItemResponse) => {
           if (error) {
             // Handle the error here if needed
             reject(error);
@@ -141,11 +153,15 @@ export default class Docker implements IRuntime {
           },
           data_item: request_item,
         },
-        (error: Error | null, runtimeResponse: any) => {
+        (error: Error | null, runtimeResponse: TransformDataItemResponse) => {
           if (error) {
             // Handle the error here if needed
             reject(error);
           } else {
+            if (runtimeResponse.transformed_data_item === undefined) {
+              throw new Error("runtimeResponse.dataItem is undefined");
+            }
+
             const responseDataItem: DataItem = {
               key: runtimeResponse.transformed_data_item.key,
               value: JSON.parse(runtimeResponse.transformed_data_item.value)
@@ -178,7 +194,7 @@ export default class Docker implements IRuntime {
           proposed_data_item: request_proposed_data_item,
           validation_data_item: request_validation_data_item
         },
-        (error: Error | null, runtimeResponse: any) => {
+        (error: Error | null, runtimeResponse: ValidateDataItemResponse) => {
           if (error) {
             // Handle the error here if needed
             reject(error);
@@ -203,7 +219,7 @@ export default class Docker implements IRuntime {
           },
           bundle: grpcBundle,
         },
-        (error: Error | null, runtimeResponse: any) => {
+        (error: Error | null, runtimeResponse: SummarizeDataBundleResponse) => {
           if (error) {
             // Handle the error here if needed
             reject(error);
@@ -224,11 +240,15 @@ export default class Docker implements IRuntime {
           },
           key: key,
         },
-        (error: Error | null, runtimeResponse: any) => {
+        (error: Error | null, runtimeResponse: NextKeyResponse) => {
           if (error) {
             // Handle the error here if needed
             reject(error);
           } else {
+            if (runtimeResponse.next_key === undefined) {
+              throw new Error("runtimeResponse.dataItem is undefined");
+            }
+
             resolve(runtimeResponse.next_key);
           }
         }
@@ -236,3 +256,9 @@ export default class Docker implements IRuntime {
     });
   }
 }
+​function getAllMethods(object:any) {
+  return Object.getOwnPropertyNames(object).filter(function(property) {
+      return typeof object[property] == 'function';
+  });
+}
+
