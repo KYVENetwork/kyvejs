41a90d091b992831a75ab5fe89f87aede7932b22
christopherbrumm <christopherbrumm2000@gmail.com>
2023-10-04 23:07:32 +0200

    update: increase max_compression_byte_size.

--- common/protocol/src/utils/constants.ts
+++ common/protocol/src/utils/constants.ts
@@ -7,8 +7,11 @@ export const REFRESH_TIME = 10 * 1000;
 // the time the node should halt if an error occurs
 export const ERROR_IDLE_TIME = 10 * 1000;
 
-// the max bundle size allowed to upload - currently 100MB
+// the max bundle size allowed to upload - currently 200MB
 export const MAX_BUNDLE_BYTE_SIZE = 200 * 1024 * 1024;
+
+// the max compression size - currently 2GB
+export const MAX_COMPRESSION_BYTE_SIZE = 2 * 10 ** 9;
 
 // a vote map to allow human readable votes
 export const VOTE = {
