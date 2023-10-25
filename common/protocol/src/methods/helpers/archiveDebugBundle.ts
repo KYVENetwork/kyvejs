a459b54df4707fbe16479080e3b62f8d0585097d
Troy Kessler <troy.kessler99@gmail.com>
2023-10-25 15:02:18 +0200

    feat: implemented zip archive creation of debug information

--- common/protocol/src/methods/helpers/archiveDebugBundle.ts
+++ common/protocol/src/methods/helpers/archiveDebugBundle.ts
@@ -1,6 +1,8 @@
-import { appendFileSync, existsSync, mkdirSync } from "fs";
+import { existsSync, mkdirSync, createWriteStream } from "fs";
 import path from "path";
 import { DataItem, standardizeError, Validator } from "../..";
+import JSZip from "jszip";
+import jsonDiff from "json-diff";
 
 /**
  * archiveDebugBundle is used for storing a bundle for debug
@@ -24,21 +26,41 @@ export function archiveDebugBundle(
       mkdirSync(path.join(this.home, `debug`), { recursive: true });
     }
 
+    const zip = new JSZip();
+
+    // save metadata which includes vote reasons and args
+    zip.file("metadata.json", JSON.stringify(metadata, null, 2));
+
+    // save current pool state including the raw bundle proposal
+    zip.file("pool.json", JSON.stringify(this.pool || {}, null, 2));
+
+    // save the proposed bundle from the uploader
+    zip.file("proposed_bundle.json", JSON.stringify(proposedBundle, null, 2));
+
+    // save the locally created bundle from this node
+    zip.file(
+      "validation_bundle.json",
+      JSON.stringify(validationBundle, null, 2)
+    );
+
+    // save the diff of the proposed and local bundle
+    zip.file(
+      "diff.txt",
+      jsonDiff.diffString(proposedBundle, validationBundle, { color: false })
+    );
+
     const storageId = this.pool?.bundle_proposal?.storage_id ?? "";
 
-    // save current pool state to specified path target
-    appendFileSync(
-      path.join(this.home, `debug`, `pool_state_${storageId}.json`),
-      JSON.stringify(this.pool || {})
-    );
-
-    // save local bundle to specified path target
-    appendFileSync(
-      path.join(this.home, `debug`, `validation_bundle_${storageId}.json`),
-      JSON.stringify(validationBundle || {})
-    );
-
-    this.logger.info("Successfully saved debug information");
+    zip
+      .generateNodeStream({ type: "nodebuffer", streamFiles: true })
+      .pipe(
+        createWriteStream(
+          path.join(this.home, `debug`, `${vote}_${storageId}.zip`)
+        )
+      )
+      .on("finish", () => {
+        this.logger.info("Successfully saved debug information");
+      });
   } catch (err) {
     this.logger.error("Failed to save debug information");
     this.logger.error(standardizeError(err));
