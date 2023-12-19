de005ddbc42c5fa162a422d9448657e1ec12eaf2
rapha <raphael.thurnherr1990@gmail.com>
2023-12-19 13:06:11 +0100

    feat: update kystrap script

--- tools/kystrap/start.sh
+++ tools/kystrap/start.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 
+# Set INTEGRATIONS_DIR
+INTEGRATIONS_DIR="$(pwd)"/integrations
+if [ ! -d "$INTEGRATIONS_DIR" ]; then
+  INTEGRATIONS_DIR="$(pwd)"/../../integrations
+fi
+if [ ! -d "$INTEGRATIONS_DIR" ]; then
+  echo "Could not find INTEGRATIONS_DIR folder"
+  exit 1
+fi
+
+# Set KYSTRAP_DIR
+KYSTRAP_DIR="$(pwd)"/tools/kystrap
+if [ ! -d "$KYSTRAP_DIR" ]; then
+  KYSTRAP_DIR="$(pwd)"/../kystrap
+fi
+
 # Build docker image
-docker build --tag kystrap .
+docker build --tag kystrap "$KYSTRAP_DIR" || exit 1
 
 # Run docker image
 docker run \
   -it                                   `# Interactive mode` \
   --rm                                  `# Remove container after run` \
   --user "$(id -u):$(id -g)"            `# Run as current user` \
-  -v $(pwd)/../../integrations:/app/out `# Mount integrations folder` \
+  -v "$INTEGRATIONS_DIR":/app/out       `# Mount integrations folder` \
   kystrap $(echo "$@")                   # Pass all arguments to kystrap
