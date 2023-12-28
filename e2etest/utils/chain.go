90798a7137de678538b451afdc243eebf7bea40f
rapha <raphael.thurnherr1990@gmail.com>
2023-12-28 17:36:08 +0100

    chore: clean up genesis file mergin and add more mnemonics

--- e2etest/utils/chain.go
+++ e2etest/utils/chain.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"cosmossdk.io/math"
 	"encoding/json"
+	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	bankTypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/icza/dyno"
@@ -14,7 +15,29 @@ import (
 const (
 	version = "v1.4.0"
 	uidGid  = "1025:1025"
+
+	// AliceMnemonic -> kyve1jq304cthpx0lwhpqzrdjrcza559ukyy3zsl2vd
+	AliceMnemonic = "worry grief loyal smoke pencil arrow trap focus high pioneer tomato hedgehog essence purchase dove pond knee custom phone gentle sunset addict mother fabric"
+	// AliceValaccountMnemonic -> kyve1aw5gtwz50g7u60geulppjwqlev2klqgvhnzu6k
+	AliceValaccountMnemonic = "artist final stage coffee coach stable quantum feed catch bridge pond like ranch steel insane hold vital horse catalog battle company suspect satoshi famous"
+	// BobMnemonic -> kyve1hvg7zsnrj6h29q9ss577mhrxa04rn94h7zjugq
+	BobMnemonic = "crash sick toilet stumble join cash erode glory door weird diagram away lizard solid segment apple urge joy annual able tank define candy demise"
+	// BobValaccountMnemonic -> kyve15azz593mwcg6k2uxdt974ax5q2j6dxa8vhnx4m
+	BobValaccountMnemonic = "rebuild resist mix bulb glass draw guess soda interest auto giggle noble cave boat wheat enact laugh bunker piano can flush stem crumble lunar"
+	// ViktorMnemonic -> kyve1khwd59xmxs26fxwc9sqkwpuy9jxym3a485rzgf
+	ViktorMnemonic = "surround burst truly again vanish warrior arctic cave share marriage rib surge"
+	// ViktorValaccountMnemonic -> kyve1fsll7rjlx49z5gnfj03pagaql3z4z2ejnalmnx
+	ViktorValaccountMnemonic = "art beyond rather game prepare depend slice entry ignore fashion replace front"
 )
+
+var Mnemonics = []string{
+	AliceMnemonic,
+	AliceValaccountMnemonic,
+	BobMnemonic,
+	BobValaccountMnemonic,
+	ViktorMnemonic,
+	ViktorValaccountMnemonic,
+}
 
 var MainnetConfig = ibc.ChainConfig{
 	Type:    "cosmos",
@@ -59,38 +82,34 @@ func mergeWithConfigOverrides(genesis map[string]interface{}) error {
 		return err
 	}
 
-	appState := jsonObj["genesis"].(map[string]interface{})["app_state"]
-	poolObj := appState.(map[string]interface{})["pool"].(map[string]interface{})
-	err = dyno.Set(genesis, poolObj, "app_state", "pool")
-	if err != nil {
-		return err
-	}
+	appState := jsonObj["genesis"].(map[string]interface{})["app_state"].(map[string]interface{})
+	for key, newValue := range appState {
+		oldValue, err := dyno.Get(genesis, "app_state", key)
+		if err != nil {
+			return err
+		}
 
-	stakerList := appState.(map[string]interface{})["stakers"].(map[string]interface{})["staker_list"]
-	err = dyno.Set(genesis, stakerList, "app_state", "stakers", "staker_list")
-	if err != nil {
-		return err
-	}
-	valaccountList := appState.(map[string]interface{})["stakers"].(map[string]interface{})["valaccount_list"]
-	err = dyno.Set(genesis, valaccountList, "app_state", "stakers", "valaccount_list")
-	if err != nil {
-		return err
-	}
+		// Assuming the values are also maps
+		oldMap, ok := oldValue.(map[string]interface{})
+		if !ok {
+			return fmt.Errorf("value is not a map[string]interface{}")
+		}
 
-	funderList := appState.(map[string]interface{})["funders"].(map[string]interface{})["funder_list"]
-	err = dyno.Set(genesis, funderList, "app_state", "funders", "funder_list")
-	if err != nil {
-		return err
-	}
-	fundingList := appState.(map[string]interface{})["funders"].(map[string]interface{})["funding_list"]
-	err = dyno.Set(genesis, fundingList, "app_state", "funders", "funding_list")
-	if err != nil {
-		return err
-	}
-	fundingStateList := appState.(map[string]interface{})["funders"].(map[string]interface{})["funding_state_list"]
-	err = dyno.Set(genesis, fundingStateList, "app_state", "funders", "funding_state_list")
-	if err != nil {
-		return err
+		newMap, ok := newValue.(map[string]interface{})
+		if !ok {
+			return fmt.Errorf("value is not a map[string]interface{}")
+		}
+
+		// Merge the old and new maps
+		for k, v := range newMap {
+			oldMap[k] = v
+		}
+
+		// Set the merged map back into the genesis map
+		err = dyno.Set(genesis, oldMap, "app_state", key)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
@@ -108,12 +127,6 @@ func ModifyGenesis(config ibc.ChainConfig, genbz []byte) ([]byte, error) {
 	balances = append(balances, bankTypes.Balance{
 		Address: "kyve1e29j95xmsw3zmvtrk4st8e89z5n72v7nf70ma4",
 		Coins:   sdk.NewCoins(sdk.NewCoin(config.Denom, math.NewInt(165_000_000_000_000))),
-	}, bankTypes.Balance{
-		Address: "kyve1jq304cthpx0lwhpqzrdjrcza559ukyy3zsl2vd", // Alice
-		Coins:   sdk.NewCoins(sdk.NewCoin(config.Denom, math.NewInt(100_000_000))),
-	}, bankTypes.Balance{
-		Address: "kyve1aw5gtwz50g7u60geulppjwqlev2klqgvhnzu6k", // Alice's valaccount
-		Coins:   sdk.NewCoins(sdk.NewCoin(config.Denom, math.NewInt(100_000_000))),
 	})
 	_ = dyno.Set(genesis, balances, "app_state", "bank", "balances")
 
