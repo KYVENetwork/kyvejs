b6c93fc44bc99c11e1535414de7685a4c666162b
rapha <raphael.thurnherr1990@gmail.com>
2024-01-18 16:52:19 +0100

    feat: add version cmd

--- tools/kysor/cmd/version.go
+++ tools/kysor/cmd/version.go
@@ -3,15 +3,23 @@ package cmd
 import (
 	"fmt"
 	"github.com/KYVENetwork/kyvejs/tools/kysor/cmd/config"
-
 	"github.com/spf13/cobra"
+	"runtime"
+	"runtime/debug"
 )
 
 var versionCmd = &cobra.Command{
 	Use:   config.VersionCmdConfig.Name,
 	Short: config.VersionCmdConfig.Short,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version called")
+		bi, ok := debug.ReadBuildInfo()
+		if ok && bi.Main.Version != "" {
+			fmt.Println(fmt.Sprintf("Kysor version: %s", bi.Main.Version))
+		}
+		fmt.Println(fmt.Sprintf("Go version: %s", runtime.Version()))
+		fmt.Println()
+		fmt.Println(fmt.Sprintf("Platform: %s", runtime.GOOS))
+		fmt.Println(fmt.Sprintf("Arch: %s", runtime.GOARCH))
 	},
 }
 
