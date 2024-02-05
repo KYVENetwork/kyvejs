90510889106e68b8d6dbb882a4edb2bc0f817d27
christopherbrumm <christopherbrumm2000@gmail.com>
2024-02-05 09:13:41 +0100

    update: remove support for public RPC providers & trace_calls.

--- integrations/evm/src/runtime.ts
+++ integrations/evm/src/runtime.ts
@@ -2,12 +2,10 @@ import { DataItem, IRuntime, Validator, VOTE } from '@kyvejs/protocol';
 import { name, version } from '../package.json';
 import { providers } from 'ethers';
 import { BlockWithTransactions, TransactionReceipt } from '@ethersproject/abstract-provider';
-import { shuffle, removeLeadingZero, chunkArray, removeOutputProperties } from "../utils/utils";
 
 // EVM config
 interface IConfig {
-  sources: string[];
-  sourceThreshold: number,
+  rpc: string;
   finality: number;
 }
 
@@ -19,22 +17,12 @@ export default class EVM implements IRuntime {
   async validateSetConfig(rawConfig: string): Promise<void> {
     const config: IConfig = JSON.parse(rawConfig);
 
-    let RPCs: string[] = [];
+    if (!config.rpc) {
+      throw new Error(`Config does not have property "rpc" defined`);
+    }
 
     if (process.env.KYVEJS_EVM_RPC) {
-      RPCs = process.env.KYVEJS_EVM_RPC!.split(",");
-    }
-
-    if (RPCs.length) {
-      config.sources = ["https://polygon-rpc.com","https://polygon.drpc.org","https://rpc.ankr.com/polygon/eb2a0f47804a2f34f4af73be124e7df49ca9ed3e0675b850409aa3affc562a63"]
-    }
-
-    if (!config.sources.length) {
-      throw new Error(`Config does not have any sources`);
-    }
-
-    if (config.sources.length < config.sourceThreshold) {
-      throw new Error(`Sources threshold ${config.sourceThreshold} not reached with ${config.sources.length} sources`)
+      config.rpc = process.env.KYVEJS_EVM_RPC;
     }
 
     if (!config.finality) {
@@ -47,7 +35,29 @@ export default class EVM implements IRuntime {
   async  getDataItem(_: Validator, key: string): Promise<any> {
     const blockWithTxs: BlockWithTransactions[] = [];
     const receipts: TransactionReceipt[] = [];
-    const traceCalls: any[] = [];
+    // const traceCalls: any[] = [];
+
+    const provider = new providers.StaticJsonRpcProvider({
+      url: this.config.rpc,
+    });
+
+    const currentHeight = await provider.getBlockNumber();
+
+    const block = await provider.getBlockWithTransactions(+key);
+
+    // only validate if current height is already 'finalized'
+    if (block.number >= currentHeight - 256) {
+      throw new Error(
+        `Finality not reached yet; waiting for next block`
+      )
+    }
+    // delete confirmations from transactions to keep data deterministic
+    block.transactions.forEach(
+      (tx: Partial<providers.TransactionResponse>) => {
+        delete tx.confirmations
+      }
+    );
+    blockWithTxs.push(block);
 
     const receiptRequestData = {
       method: 'eth_getBlockReceipts',
@@ -56,59 +66,32 @@ export default class EVM implements IRuntime {
       jsonrpc: '2.0',
     };
 
-    const sources = shuffle(["https://polygon-rpc.com", "https://polygon.drpc.org", "https://rpc.ankr.com/polygon"]);
-    for (let i = 0; i < 1; i++) {
-      const provider = new providers.StaticJsonRpcProvider({
-        url: sources[i],
-      });
+    // retrieve all transaction receipts for the key
+    const receipt = await provider.send(receiptRequestData.method, receiptRequestData.params);
+    receipts.push(receipt);
 
-      const currentHeight = await provider.getBlockNumber();
-
-      console.log("current height", currentHeight)
-
-      const block = await provider.getBlockWithTransactions(+key);
-
-      // only validate if current height is already 'finalized'
-      if (block.number >= currentHeight - 256) {
-        throw new Error(
-          `Finality not reached yet; waiting for next block`
-        )
-      }
-      // delete confirmations from transactions to keep data deterministic
-      block.transactions.forEach(
-        (tx: Partial<providers.TransactionResponse>) => {
-          delete tx.confirmations
-        }
-      );
-      blockWithTxs.push(block);
-
-      // retrieve all transaction receipts for the key
-      const receipt = await provider.send(receiptRequestData.method, receiptRequestData.params);
-      receipts.push(receipt);
-
-      const chunkedTransactions = chunkArray(blockWithTxs[0].transactions, 4);
-
-      const chunkedTraceCalls: any[] = [];
-      // Process each chunk
-      for (const chunk of chunkedTransactions) {
-        const traceParams = chunk.map((tx: any) => [
-          {
-            from: tx.from,
-            to: tx.to,
-            value: removeLeadingZero(tx.value.toHexString()),
-          },
-          ['trace'],
-        ]);
-
-        // Send trace call request for each chunk
-        const traceCall = await provider.send('trace_callMany', [traceParams, 'latest']);
-
-        traceCall.forEach((t: any) => {
-          chunkedTraceCalls.push(t);
-        })
-      }
-      traceCalls.push(chunkedTraceCalls)
-    }
+    // const chunkedTransactions = chunkArray(blockWithTxs[0].transactions, 4);
+    //
+    // const chunkedTraceCalls: any[] = [];
+    // // Process each chunk
+    // for (const chunk of chunkedTransactions) {
+    //   const traceParams = chunk.map((tx: any) => [
+    //     {
+    //       from: tx.from,
+    //       to: tx.to,
+    //       value: removeLeadingZero(tx.value.toHexString()),
+    //     },
+    //     ['trace'],
+    //   ]);
+    //
+    //   // Send trace call request for each chunk
+    //   const traceCall = await provider.send('trace_callMany', [traceParams, 'latest']);
+    //
+    //   traceCall.forEach((t: any) => {
+    //     chunkedTraceCalls.push(t);
+    //   })
+    // }
+    // traceCalls.push(chunkedTraceCalls)
 
     // check if results from the different sources match
     if (
@@ -121,18 +104,18 @@ export default class EVM implements IRuntime {
     ) {
       throw new Error(`Sources returned different receipts`);
     }
-    if (
-      !traceCalls.every((t) => JSON.stringify(t) === JSON.stringify(traceCalls[0]))
-    ) {
-      throw new Error(`Sources returned different traceCalls`);
-    }
+    // if (
+    //   !traceCalls.every((t) => JSON.stringify(t) === JSON.stringify(traceCalls[0]))
+    // ) {
+    //   throw new Error(`Sources returned different traceCalls`);
+    // }
 
     return {
       key,
       value: {
         block: blockWithTxs[0],
         receipts: receipts[0],
-        traceCalls: traceCalls[0]
+        // traceCalls: traceCalls[0]
       },
     };
   }
@@ -159,15 +142,17 @@ export default class EVM implements IRuntime {
     ) {
       return VOTE.VOTE_TYPE_VALID;
     }
-    // Remove non-deterministic data
-    const modifiedValidationDataItem = removeOutputProperties(validationDataItem.value.traceCalls, ["output", "accessList", "address", "value"]);
-    const modifiedProposedDataItem = removeOutputProperties(proposedDataItem.value.traceCalls, ["output", "accessList", "address", "value"])
 
-  if (
-      JSON.stringify(modifiedProposedDataItem) === JSON.stringify(modifiedValidationDataItem)
-    ) {
-      return VOTE.VOTE_TYPE_ABSTAIN;
-    }
+  // Remove non-deterministic data
+  //   const modifiedValidationDataItem = removeOutputProperties(validationDataItem.value.traceCalls, ["output", "accessList", "address", "value"]);
+  //   const modifiedProposedDataItem = removeOutputProperties(proposedDataItem.value.traceCalls, ["output", "accessList", "address", "value"])
+  //
+  // if (
+  //     JSON.stringify(modifiedProposedDataItem) === JSON.stringify(modifiedValidationDataItem)
+  //   ) {
+  //     return VOTE.VOTE_TYPE_ABSTAIN;
+  //   }
+
     return VOTE.VOTE_TYPE_INVALID;
   }
 
