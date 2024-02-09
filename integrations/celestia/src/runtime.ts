ac9e718836d58ea0af6cbf88b61ad0498298cecf
christopherbrumm <christopherbrumm2000@gmail.com>
2024-02-09 12:13:24 +0100

    update: add Celestia.

---
+++ integrations/celestia/src/runtime.ts
@@ -0,0 +1,96 @@
+import { DataItem, IRuntime, Validator, VOTE } from '@kyvejs/protocol';
+import { name, version } from '../package.json';
+import axios from 'axios';
+
+// Celestia config
+interface IConfig {
+  namespace: string;
+  rpc: string;
+}
+
+export default class Celestia implements IRuntime {
+  public name = name;
+  public version = version;
+  public config!: IConfig;
+
+  async validateSetConfig(rawConfig: string): Promise<void> {
+    const config: IConfig = JSON.parse(rawConfig);
+
+    if (!config.namespace) {
+      throw new Error(`Config does not have property "namespace" defined`);
+    }
+
+    if (!config.rpc) {
+      throw new Error(`Config does not have property "rpc" defined`);
+    }
+
+    if (process.env.KYVEJS_CELESTIA_RPC) {
+      config.rpc = process.env.KYVEJS_CELESTIA_RPC;
+    }
+
+    this.config = config;
+  }
+
+  async getDataItem(_: Validator, key: string): Promise<DataItem> {
+    let blobs;
+
+    // fetch GetAll from namespace
+    axios.post('http://127.0.0.1:26658', {
+      id: 1,
+      jsonrpc: '2.0',
+      method: 'blob.GetAll',
+      params: [
+        +key,
+        [
+          this.config.namespace
+        ]
+      ]
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.CELESTIA_NODE_AUTH_TOKEN}`
+      }
+    })
+      .then(response => {
+        blobs = response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+    return { key, value: blobs };
+  }
+
+  async prevalidateDataItem(_: Validator, item: DataItem): Promise<boolean> {
+    return true;
+  }
+
+  async transformDataItem(_: Validator, item: DataItem): Promise<DataItem> {
+    return item;
+  }
+
+  async validateDataItem(
+    v: Validator,
+    proposedDataItem: DataItem,
+    validationDataItem: DataItem
+  ): Promise<number> {
+    if (
+      JSON.stringify(proposedDataItem) === JSON.stringify(validationDataItem)
+    ) {
+      // vote valid if data matches
+      return VOTE.VOTE_TYPE_VALID;
+    }
+    // vote invalid if data does not match
+    return VOTE.VOTE_TYPE_INVALID;
+  }
+
+  async summarizeDataBundle(_: Validator, bundle: DataItem[]): Promise<string> {
+    // use latest block height as bundle summary
+    return ""
+  }
+
+  async nextKey(_: Validator, key: string): Promise<string> {
+    // the next key is always current block height + 1
+    return (parseInt(key) + 1).toString();
+  }
+}
