6870f82a08186859f3b1cbbcb644680f18486391
rapha <raphael.thurnherr1990@gmail.com>
2024-02-15 13:54:13 -0300

    chore: delete typescript stuff in kysor

--- tools/kysor/src/utils/constants.ts
+++
@@ -1,7 +0,0 @@
-import os from "os";
-
-export const USER_HOME = process.env.HOME!;
-export const KYSOR_DIR = ".kysor";
-export const PLATFORM = os.platform() === "darwin" ? "macos" : os.platform();
-export const ARCH = os.arch();
-export const FILE_ACCESS = "600";
