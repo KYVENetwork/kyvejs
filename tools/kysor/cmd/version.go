091141211d4fcc59b065b5a7a98dc93679e48e47
rapha <raphael.thurnherr1990@gmail.com>
2024-02-19 14:24:38 -0300

    chore: update kysor version handling

--- tools/kysor/cmd/version.go
+++ tools/kysor/cmd/version.go
@@ -2,22 +2,21 @@ package cmd
 
 import (
 	"fmt"
-	"runtime"
-	"runtime/debug"
-
 	"github.com/spf13/cobra"
+	"runtime"
 )
+
+var Version = "development"
+var Commit = "none"
 
 func versionCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "version",
 		Short: "Show KYSOR version",
 		Run: func(cmd *cobra.Command, args []string) {
-			bi, ok := debug.ReadBuildInfo()
-			if ok && bi.Main.Version != "" {
-				fmt.Printf("Kysor version: %s\n", bi.Main.Version)
-			}
+			fmt.Printf("Kysor version: %s\n", Version)
 			fmt.Printf("Go version: %s\n", runtime.Version())
+			fmt.Printf("Git commit: %s\n", Commit)
 			fmt.Println()
 			fmt.Printf("Platform: %s\n", runtime.GOOS)
 			fmt.Printf("Arch: %s\n", runtime.GOARCH)
