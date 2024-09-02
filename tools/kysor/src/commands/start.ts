99cc0c510fa4554ec7947ef5dcbe819fc4b0f376
Troy Kessler <troy.kessler99@gmail.com>
2024-09-02 15:47:04 +0200

    chore: added dry-run flag to kysor

--- tools/kysor/src/commands/start.ts
+++ tools/kysor/src/commands/start.ts
@@ -15,6 +15,15 @@ start
     "Specify the path to an .env file which should be used when starting a binary"
   )
   .option("--debug", "Run the validator node in debug mode")
+  .option(
+    "--dry-run",
+    "Run the node without uploading or voting on bundles so the operator can test his setup before joining as a validator."
+  )
+  .option(
+    "--dry-run-bundles <number>",
+    "Specify the number of bundles that should be tested before the node properly exits. If zero the node will run indefinitely [default = 0]",
+    "0"
+  )
   .action(async (options) => {
     await run(options);
   });
