bb49803ad41719947f83b44a8bea7f5c2cefdc11
charmful0x <charmful0x@gmail.com>
2025-09-01 11:24:22 +0200

    feat: add Load S3 storage provider

--- common/protocol/src/reactors/storageProviders/index.ts
+++ common/protocol/src/reactors/storageProviders/index.ts
@@ -3,6 +3,7 @@ import { Arweave } from "./Arweave";
 import { Bundlr } from "./Bundlr";
 import { Kyve } from "./Kyve";
 import { Turbo } from "./Turbo";
+import { Load } from "./Load";
 import { NoStorageProvider } from "./NoStorageProvider";
 
 /**
@@ -10,10 +11,10 @@ import { NoStorageProvider } from "./NoStorageProvider";
  * from the specified id. Current storage providers are:
  *
  * 0 - NoStorageProvider
- * 1 - Arweave
  * 2 - Bundlr
  * 3 - Kyve
  * 4 - Turbo
+ * 5 - Load
  * x - NoStorageProvider (default)
  *
  * @method storageProviderFactory
@@ -29,6 +30,8 @@ export function storageProviderFactory(this: Validator): IStorageProvider {
       return new Kyve(this.chainId, this.poolId, this.staker, this.poolAccount);
     case 4:
       return new Turbo(this.storagePriv || this.poolAccount);
+    case 5:
+      return new Load(this.storagePriv);
     default:
       return new NoStorageProvider();
   }
